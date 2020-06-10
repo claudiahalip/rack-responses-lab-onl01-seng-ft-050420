c29fe094299f0f0c60f7373f43e54673a8d2c537
claudia v <claudia4ro@yahoo.com>
2020-06-10 22:06:49 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -5,7 +5,7 @@ class Application
   def call(env)
     resp = Rack::Response.new
     time = Time.now.hour
-    if time["time"].to_i <= 12
+    if time <= 12
       resp.write " Good Morning!"
       
     else
